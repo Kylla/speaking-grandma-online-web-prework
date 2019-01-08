de15c470d959f392e9c5912e40971cec48aa0ad2
Kylla Harris <kylla.harris@gmail.com>
2019-01-08 19:42:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 speak_to_grandma = "I LOVE YOU GRANDMA!"
 
-if speech.upcase == "I LOVE YOU GRANDMA!"
+if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   
 elsif speech != "Hi"
