e7bc261ea44711d36b097348f861e29db5ee049a
Kylla Harris <kylla.harris@gmail.com>
2019-01-08 18:02:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,15 +17,14 @@ speak_to_grandma = "I LOVE YOU GRANDMA!"
 if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   
-elsif speak_to_grandma == "Hi"
-  puts "HUH?! SPEAK UP, SONNY!"
+# elsif speak_to_grandma == "Hi"
+ # puts "HUH?! SPEAK UP, SONNY!"
   
-elsif speak_to_grandma == "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
+ # puts "HUH?! SPEAK UP, SONNY!"
   
-elsif speak_to_grandma == "WHAT?"
-  puts "NO, NOT SINCE 1938!"
+#elsif speak_to_grandma == "WHAT?"
+#  puts "NO, NOT SINCE 1938!"
  
-else 
-  puts "NO, NOT SINCE 1938!"
+#else 
+ # puts "NO, NOT SINCE 1938!"
 end
