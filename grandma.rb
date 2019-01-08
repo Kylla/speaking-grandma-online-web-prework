c449e332342649732f1bad02caeddcfbdf544777
Kylla Harris <kylla.harris@gmail.com>
2019-01-08 19:51:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
-      return 'I LOVE YOU TOO PUMPKIN!'
+      puts 'I LOVE YOU TOO PUMPKIN!'
   
     elsif phrase.upcase == phrase
-      return 'NO, NOT SINCE 1938!'
+      puts 'NO, NOT SINCE 1938!'
       
     else
-      return 'HUH?! SPEAK UP, SONNY!'
+      puts 'HUH?! SPEAK UP, SONNY!'
       
     end
   
